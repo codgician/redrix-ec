b0d8d7414e773455c2e9d72c3137366d79ff5f99
Rob Barnes <robbarnes@google.com>
2026-04-14 16:25:24 -0600

    dedede: npcx: reduce USB_PD_INT_STORM_MAX
    reduce the max possible PD interrupt rate to avoid hitting
    watchdog on dedede. This max was previously only applied to IT8320
    variants. This change applies the max to all Dedede boards.
    
    BUG=b:497823439
    TEST=no watchdog on bugzzy while simulating int storm on both ports
    
    Change-Id: I969c951dc1e4e65032764da603888a4448e435c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7763376
    Reviewed-by: Boris Mittelberg <bmbm@google.com>
    Tested-by: Rob Barnes <robbarnes@google.com>
    Commit-Queue: Rob Barnes <robbarnes@google.com>

--- baseboard/dedede/baseboard.h
+++ baseboard/dedede/baseboard.h
@@ -249,10 +249,8 @@
 #define CONFIG_CMD_CHARGEN
 #endif
 
-#if defined(VARIANT_DEDEDE_EC_IT8320)
 #undef CONFIG_USB_PD_INT_STORM_MAX
 #define CONFIG_USB_PD_INT_STORM_MAX 280
-#endif
 
 /* Define typical operating power and max power. */
 #define CONFIG_USB_PD_MAX_CURRENT_MA 3000
