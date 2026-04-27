df106cf26d57c8ab5fadd84499fdd70e08bd4d5e
Rob Barnes <robbarnes@google.com>
2026-04-27 16:43:54 +0000

    npcx: i2c: Restrict stuck SDAST check to SMB_IDLE state
    The stuck SDAST check introduced in CL:7609361
    was too broad and triggered during normal write operations, causing
    spurious timeouts.
    
    Restrict the check to only trigger when the driver state is SMB_IDLE,
    as originally intended to prevent interrupt storms when the driver is
    supposed to be inactive.
    
    BUG=b:441975147
    TEST=Verified on bugzzy that false positive timeouts are gone.
    
    Change-Id: Ie1fb5c976adc7fcbaf06189725d5c88ae30aac45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7796555
    Commit-Queue: Rob Barnes <robbarnes@google.com>
    Reviewed-by: Fabio Baltieri <fabiobaltieri@google.com>
    Tested-by: Rob Barnes <robbarnes@google.com>

--- chip/npcx/i2c.c
+++ chip/npcx/i2c.c
@@ -849,7 +849,7 @@ static void i2c_controller_int_handler(int controller)
 		 * before executing error recovery.
 		 */
 		if (IS_BIT_SET(NPCX_SMBST(controller), NPCX_SMBST_SDAST) &&
-		    p_status->oper_state != SMB_WRITE_SUSPEND) {
+		    p_status->oper_state == SMB_IDLE) {
 #if DEBUG_I2C
 			cprints(CC_I2C, "i2c %d unknown state %d, error %d",
 				controller, p_status->oper_state,
